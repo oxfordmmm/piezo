6b3392b54868b96542181ccb85fb4894dcda0026
Philip Fowler <philipwfowler@gmail.com>
2019-05-23 20:48:55 +0100

    removed code that is now in vasta

--- piezo/__init__.py
+++ piezo/__init__.py
@@ -8,4 +8,3 @@ from .gene import Gene
 from .gene_collection import GeneCollection
 from .catalogue import ResistanceCatalogue
 from .vcf_measurement import VCFMeasurement
-from .genome import Genome
