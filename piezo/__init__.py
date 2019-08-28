3b83e57e516d507a19dda2c7c960547cf814526f
Philip Fowler <philipwfowler@gmail.com>
2019-08-28 09:55:07 +0200

    removed objects that have moved to gumpy

--- piezo/__init__.py
+++ piezo/__init__.py
@@ -4,9 +4,5 @@
 
 __version__="0.0.1"
 
-from .genotype import Genotype
-from .genome import Genome
-from .gene import Gene
-from .gene_collection import GeneCollection
 from .catalogue import ResistanceCatalogue
 from .vcf_measurement import VCFMeasurement
