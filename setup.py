ce6c99156f3a4049c5086e223ec9e7d350408533
Philip Fowler <philipwfowler@gmail.com>
2019-12-04 11:30:14 +0000

    Revert "tided setup"
    This reverts commit ab7de868c6b3b2333679c9b8d60ca85818060fce.

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ setup(
     version=__version__,
     author='Philip W Fowler',
     author_email="philip.fowler@ndm.ox.ac.uk",
-    description="Predict the effect of a genetic mutation on the effect of an antibiotic using a supplied AMR catalogue.",
+    description="Antibiotic suscetibility predictions from a VCF file.",
     url="https://github.com/philipwfowler/piezo",
     packages=['piezo'],
+    package_data={'': ['../config/*']},
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent"  ],
